b666a2927ec3ee74a35676689f44679b60cb40f3
kumereshN <kumeresh6@hotmail.com.com>
2022-10-24 18:01:52 +0800

    updates to chapter 6 uniqr

--- 06_uniqr/uniqr/src/main.rs
+++ 06_uniqr/uniqr/src/main.rs
@@ -1,3 +1,8 @@
+mod lib;
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = uniqr::get_args().and_then(uniqr::run) {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
 }
