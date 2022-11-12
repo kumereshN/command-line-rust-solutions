e6eef697f151939b0c42fb3e8aa0f501a4f0e501
kumereshN <kumeresh6@hotmail.com.com>
2022-11-12 18:18:04 +0800

    done with chapter 7

--- 07_findr/findr/src/lib.rs
+++ 07_findr/findr/src/lib.rs
@@ -2,6 +2,7 @@ use crate::EntryType::*;
 use clap::{App, Arg};
 use regex::Regex;
 use std::error::Error;
+use walkdir::{DirEntry, WalkDir};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -60,6 +61,7 @@ pub fn get_args() -> MyResult<Config> {
                     Regex::new(&name)
                         .map_err(|_| format!("Invalid --name \"{}\"", name))
                 })
+                // Turbo-fish
                 .collect::<Result<Vec<_>, _>>()
         })
         .transpose()?
@@ -74,7 +76,7 @@ pub fn get_args() -> MyResult<Config> {
                     "f" => File,
                     "l" => Link,
                     _ => unreachable!("Invalid type"),
-            })
+                })
                 .collect()
         })
         .unwrap_or_default();
@@ -87,6 +89,42 @@ pub fn get_args() -> MyResult<Config> {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:?}", config);
+    let type_filter = |entry: &DirEntry| {
+        config.entry_types.is_empty()
+            || config
+                .entry_types
+                .iter()
+                .any(|entry_type| match entry_type {
+                    Link => entry.path_is_symlink(),
+                    Dir => entry.file_type().is_dir(),
+                    File => entry.file_type().is_file(),
+                })
+    };
+
+    let name_filter = |entry: &DirEntry| {
+        config.names.is_empty()
+            || config
+                .names
+                .iter()
+                .any(|re| re.is_match(&entry.file_name().to_string_lossy()))
+    };
+
+    for path in &config.paths {
+        let entries = WalkDir::new(path)
+            .into_iter()
+            .filter_map(|e| match e {
+                Err(e) => {
+                    eprintln!("{}", e);
+                    None
+                }
+                Ok(entry) => Some(entry),
+            })
+            .filter(type_filter)
+            .filter(name_filter)
+            .map(|entry| entry.path().display().to_string())
+            .collect::<Vec<_>>();
+
+        println!("{}", entries.join("\n"));
+    }
     Ok(())
 }
